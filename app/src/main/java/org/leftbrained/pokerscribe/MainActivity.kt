a1194c5a92cb7c969837d6bbbb36044843e18969
Chris Testroet <christestroet@gmail.com>
2022-03-13 16:44:31 -0700

    Enable data binding

--- app/src/main/java/org/leftbrained/pokerscribe/MainActivity.kt
+++ app/src/main/java/org/leftbrained/pokerscribe/MainActivity.kt
@@ -2,10 +2,16 @@ package org.leftbrained.pokerscribe
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+
+import org.leftbrained.pokerscribe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
